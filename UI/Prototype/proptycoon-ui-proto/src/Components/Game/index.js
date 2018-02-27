d617344012a648da221e791ae2b103200acd0a61
Tom <tomchescreations@gmail.com>
2018-02-27 13:48:02 +0000

    New directory for freeform diagrams.

--- UI/Prototype/proptycoon-ui-proto/src/Components/Game/index.js
+++ UI/Prototype/proptycoon-ui-proto/src/Components/Game/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import Board from './Components/Board';
 import PlayerLegend from './Components/PlayerLegend';
+import GameModel from '../../Models/Game';
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            model = new GameModel()
+        }
+    }
+
     render() {
         return (
             <div>
