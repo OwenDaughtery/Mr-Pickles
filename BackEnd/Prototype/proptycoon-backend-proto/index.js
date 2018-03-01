aaa7e11d956be529a7f5dcb206dc035cd7b7cd13
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-03-01 15:45:33 +0000

    Update index.js

--- BackEnd/Prototype/proptycoon-backend-proto/index.js
+++ BackEnd/Prototype/proptycoon-backend-proto/index.js
@@ -1,9 +1,9 @@
 // index.js
 require('import-export');
-require('./Dice.js');
-require('./Board.js');
-require('./Space.js');
-require('./Player.js');
-require('./Game.js');
+require('./src/Dice.js');
+require('./src/Board.js');
+require('./src/Space.js');
+require('./src/Player.js');
+require('./src/Game.js');
 
 //
