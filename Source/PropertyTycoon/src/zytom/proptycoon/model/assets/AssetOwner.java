478f0783920b0726868fbca5777396344b9219ed
Unknown <tomchescreations@gmail.com>
2018-04-06 23:34:41 +0100

    Wroking on asset owner

--- Source/PropertyTycoon/src/zytom/proptycoon/model/assets/AssetOwner.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/assets/AssetOwner.java
@@ -3,10 +3,35 @@
  */
 package zytom.proptycoon.model.assets;
 
+import java.util.ArrayList;
+import zytom.proptycoon.model.card.Card;
+import zytom.proptycoon.model.card.PotLuckCard;
+import zytom.proptycoon.model.card.PropertyCard;
+
 /**
- *
+ * An entity that can own assets and therefor can
+ * take part in transactions.
+ * (Players and the bank).
  * @author Tom Chesters
  */
 public interface AssetOwner {
+    /**
+     * @return The amount of money this asset owner is in possesion of.
+     */
+    public int getBalance();
     
+    /**
+     * @return An array list containing (copies) of all the cards that this asset owner is in possesion of.
+     */
+    public ArrayList<Card> getCards();
+    
+    /**
+     * @return An array list containing (copies) of all the pot luck cards that this asset owner is in possesion of.
+     */
+    public ArrayList<PotLuckCard> getPotLuckCards();
+    
+    /**
+     * @return An array list containing (copies) of all the property cards that this asset owner is in possesion of.
+     */
+    public ArrayList<PropertyCard> getProperties();
 }
