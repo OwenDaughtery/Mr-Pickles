82b6581785ebd3ae8131318f7c56a5697c6d55b5
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 15:11:32 +0100

    Update PropertyCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PropertyCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PropertyCell.java
@@ -2,12 +2,15 @@
  * PropertyTycoon Application by Zytom
  */
 package zytom.proptycoon.model.cell;
+import zytom.proptycoon.model.card.PropertyCard;
 
 /**
  *
  * @author Tom Chesters
  */
 public abstract class PropertyCell extends Cell {
+    
+    private PropertyCard associatedCard;
     
     /**
      * @author Zenos Pavlakou
@@ -16,6 +19,26 @@ public abstract class PropertyCell extends Cell {
      */
     public PropertyCell(String titleOfPropertyCell) {
         super(titleOfPropertyCell);
+    }
+    
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @param associatedCard The property card associated with this property
+     * 
+     * When creating the associated property card, how will constructor be initialized?
+     */
+    public void setAssociatedCard(PropertyCard associatedCard) {
+        this.associatedCard = associatedCard;
+    }
+    
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @return The property card associated with this property
+     */
+    public PropertyCard getAssociatedCard() {
+        return this.associatedCard;
     }
     
 }
