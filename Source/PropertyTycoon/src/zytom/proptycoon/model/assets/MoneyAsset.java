08580c9a6eb7b9fc8d6c85b4514f5acec934e8aa
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 12:00:52 +0100

    Update MoneyAsset.java
    Added the features a money asset needs.

--- Source/PropertyTycoon/src/zytom/proptycoon/model/assets/MoneyAsset.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/assets/MoneyAsset.java
@@ -9,4 +9,23 @@ package zytom.proptycoon.model.assets;
  */
 public class MoneyAsset extends Asset {
     
+    private int assetValue;
+    
+    /**
+     * Constructor will assign the value of the money asset.
+     * 
+     * @author Zenos Pavlakou
+     */
+    public MoneyAsset(int assetValue) {
+        this.assetValue = assetValue;
+    }
+    
+     /**
+     * @author Zenos Pavlakou
+     * 
+     * @return the value of the money asset
+     */
+    public int getValue() {
+        return this.assetValue;
+    }
 }
