49fcaeb976e41ac93451bfc91173b6d203026426
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:14:13 +0100

    Update GoToJailCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/GoToJailCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/GoToJailCell.java
@@ -1,4 +1,14 @@
 package zytom.proptycoon.model.cell;
 
 public class GoToJailCell extends Cell{
+    
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @param title The title of the cell
+     */
+    public GoToJailCell(String title) {
+        super("Go To Jail");
+    }
+    
 }
