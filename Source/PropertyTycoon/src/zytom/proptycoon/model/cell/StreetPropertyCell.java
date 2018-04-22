15219415fc49b501d83716e448c4db2be8d19e65
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 15:16:13 +0100

    Update StreetPropertyCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/StreetPropertyCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/StreetPropertyCell.java
@@ -2,6 +2,9 @@ package zytom.proptycoon.model.cell;
 
 public class StreetPropertyCell extends PropertyCell {
 
+    private int numberOfHouses;
+    private int numberOfHotels;
+    
     /**
      * @author Zenos Pavlakou
      * 
@@ -9,5 +12,29 @@ public class StreetPropertyCell extends PropertyCell {
      */
     public StreetPropertyCell(String titleOfStreetProperty) {
         super(titleOfStreetProperty);
+        this.numberOfHouses = 0;
+        this.numberOfHotels = 0;
+    }
+    
+//    public boolean validateHouseAndHotelNumbers() {
+//        ?????????
+//    }
+    
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @return The number of houses on this cell
+     */
+    public int getNumberOfHouses() {
+        return this.numberOfHouses;
+    }
+  
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @return The number of hotels on this cell
+     */
+    public int getNumberOfHotels() {
+        return this.numberOfHotels;
     }
 }
