cbbf767b4a65173206c5bb01b14fac9d2af4d784
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:18:59 +0100

    Update PassingJailCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PassingJailCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PassingJailCell.java
@@ -1,4 +1,11 @@
 package zytom.proptycoon.model.cell;
 
 public class PassingJailCell extends Cell {
+
+    /**
+     * @author Zenos Pavlakou
+     */
+    public PassingJailCell() {
+        super("Passing Jail");
+    }
 }
