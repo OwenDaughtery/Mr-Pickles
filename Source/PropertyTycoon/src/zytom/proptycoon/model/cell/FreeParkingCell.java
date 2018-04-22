85d905e459e82fc6b975d9e2e2122e2b07aae0df
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:18:04 +0100

    Update FreeParkingCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/FreeParkingCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/FreeParkingCell.java
@@ -1,4 +1,11 @@
 package zytom.proptycoon.model.cell;
 
 public class FreeParkingCell extends Cell {
+
+    /**
+     * @author Zenos Pavlakou
+     */
+    public FreeParkingCell() {
+        super("Free Parking");
+    }
 }
