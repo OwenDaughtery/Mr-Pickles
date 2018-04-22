1664d1d051977f4129065014e009c6696a4f3047
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:20:23 +0100

    Update PropertyCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PropertyCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/PropertyCell.java
@@ -9,4 +9,13 @@ package zytom.proptycoon.model.cell;
  */
 public abstract class PropertyCell extends Cell {
     
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @param titleOfPropertyCell The name of the cell
+     */
+    public PropertyCell(String titleOfPropertyCell) {
+        super(titleOfPropertyCell);
+    }
+    
 }
