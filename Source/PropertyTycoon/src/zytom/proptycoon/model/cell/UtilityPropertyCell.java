1f0bf37c3a7797540e47fc4738f0815af72465d8
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:27:12 +0100

    Update UtilityPropertyCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/UtilityPropertyCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/UtilityPropertyCell.java
@@ -1,4 +1,13 @@
 package zytom.proptycoon.model.cell;
 
 public class UtilityPropertyCell extends PropertyCell {
+
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @param titleOfUtilityProperty The name of the utility property
+     */
+    public UtilityPropertyCell(String titleOfUtilityProperty) {
+        super(titleOfUtilityProperty);
+    }
 }
