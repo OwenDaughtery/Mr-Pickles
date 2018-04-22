7e18a503b2c99c5c0bb69f79e21ab51d832f52d6
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-04-22 14:25:44 +0100

    Update StreetPropertyCell.java

--- Source/PropertyTycoon/src/zytom/proptycoon/model/cell/StreetPropertyCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/cell/StreetPropertyCell.java
@@ -1,4 +1,13 @@
 package zytom.proptycoon.model.cell;
 
 public class StreetPropertyCell extends PropertyCell {
+
+    /**
+     * @author Zenos Pavlakou
+     * 
+     * @param titleOfStreetProperty The name of the street property
+     */
+    public StreetPropertyCell(String titleOfStreetProperty) {
+        super(titleOfStreetProperty);
+    }
 }
