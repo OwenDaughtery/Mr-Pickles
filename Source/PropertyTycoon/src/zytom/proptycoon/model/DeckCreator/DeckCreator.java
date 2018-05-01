5fe0b4d65c01d030ed2a1739272ed396a4a781fb
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-01 18:11:40 +0100

    Removed try catch clause because game can't continue without card data

--- Source/PropertyTycoon/src/zytom/proptycoon/model/DeckCreator/DeckCreator.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/DeckCreator/DeckCreator.java
@@ -11,29 +11,36 @@ import java.util.Scanner;
  */
 public class DeckCreator {
     
-    public static ArrayList<String[]> initDeck(String nameOfCSV) {
+    /**
+     * Method creates an ArrayList of length 3 String arryas, each index of the String
+     * array cointains the first, second and third feature of each row in the csv file
+     * i.e. description, action and value. 
+     * 
+     * @param nameOfCSV The name of the csv document containing the data of the cards.
+     * @return The ArrayList of String arrays if the csv file can be found. 
+     * @throws java.io.FileNotFoundException 
+     */
+    public static ArrayList<String[]> initDeck(String nameOfCSV) throws FileNotFoundException {
         ArrayList<String[]> deck = new ArrayList<>();
         String[] currentParams = new String[3];
         int index = 0;
         String path = "./" + nameOfCSV; //path may need fixing
         File file = new File(path);
-        try{
-            Scanner inputStream = new Scanner(file);
-            inputStream.useDelimiter(">>");
-            while(inputStream.hasNext()) {
-                currentParams[index] = inputStream.next();
-                index++;
-                if(index == 3) {
-                    deck.add(currentParams);
-                    currentParams = new String[3];
-                    index = 0;
-                }
+        Scanner inputStream = new Scanner(file);
+        inputStream.useDelimiter(">>");
+        while(inputStream.hasNext()) {
+            currentParams[index] = inputStream.next();
+            index++;
+            if(index == 3) {
+                deck.add(currentParams);
+                currentParams = new String[3];
+                index = 0;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
         Collections.shuffle(deck);
         return deck;
+        
+        
     }
 } 
 
