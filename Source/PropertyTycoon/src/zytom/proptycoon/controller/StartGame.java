1c9e34e1ba498ba25fe64588893723653eb86877
Unknown <aymanbfree@gmail.com>
2018-05-02 18:49:41 +0100

    Auto stash before merge of "master" and "origin/master"
    StartGame Controller for turn sequence made, clock made

--- Source/PropertyTycoon/src/zytom/proptycoon/controller/StartGame.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/controller/StartGame.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import zytom.proptycoon.model.Game;
 import zytom.proptycoon.model.Player;
 import zytom.proptycoon.model.assets.AssetOwner;
+import zytom.proptycoon.model.cell.Cell;
+import javax.swing.Timer;
 
 import java.util.ArrayList;
 
@@ -17,52 +19,90 @@ public class StartGame {
 
     private Game game;
     private int playerTurn;
+    private int doubles = 0;
+    private Timer t = new Timer(1000, new ActionListener() {
 
-    public StartGame(ArrayList<Player> players, Player startingPlayer) throws Game.PlayerNumberException {
+        @Override
+        public void actionPerformed(ActionEvent a) {
+            time++;
+        }
+    });
+
+    int time = 0;
+
+    public StartGame(ArrayList<Player> players, Player startingPlayer) throws Game.PlayerNumberException, AssetOwner.AssetNotFoundException {
         this.game = new Game(players, startingPlayer);
         this.playerTurn = players.indexOf(startingPlayer);
 
+        t.start();
+        this.turn();
+
+    }
+
+    public int getTime(){
+        return time;
     }
 
     public void turn() throws AssetOwner.AssetNotFoundException {
         Player currentPlayer = Game.getCurrentPlayer();
-        int doubles = 0;
         if (currentPlayer.getPosition() == 40) {
             if (currentPlayer.getTurnsInJail() > 1) {
                 currentPlayer.moveTo(10, false);
+                //end Turn Button
             } else{
                 currentPlayer.setTurnsInJail(currentPlayer.getTurnsInJail()+1);
+                //end Turn Button
             }
         } else {
-            //Add while loop for doubles and shit.
-                // ActionListener Button 'roll' rolls the dice
-                roll.addActionListener((ActionEvent e) -> {
-                    boolean isDouble = rollDice();
-
-                });
-                //Other methods in turn.
-
+            //Roll Dice Button
         }
+    }
 
-        // ActionListener Button 'endTurn'
-        endTurn.addActionListener((ActionEvent e) -> {
-            playerTurn = playerTurn++ % 6;
+    public void landOnCell() throws Board.CellNotFoundException {
+        Player currentPlayer = Game.getCurrentPlayer();
+        int cellNumber = currentPlayer.getPosition();
+        Cell currentCell = Game.getBoard().getCell(cellNumber);
+        //Do cell action and button and shit
+    }
+
+
+
+    public void retireAction( button){
+        button.addActionListener((ActionEvent e) -> {
+            Player currentPlayer = Game.getCurrentPlayer();
+            playerTurn = playerTurn++ % Game.getPlayers().size();
             Game.setCurrentPlayer(Game.getPlayers().get(playerTurn));
+            Game.getPlayers().remove(currentPlayer);
+            turn();
         });
     }
 
-    public boolean rollDice() {
-        boolean doubles = false;
-        Player currentPlayer = Game.getCurrentPlayer();
-        Game.getDice().roll();
-        int dice1 = Game.getDice().getFirstValue();
-        int dice2 = Game.getDice().getSecondValue();
-        if (dice1 == dice2) {
-            doubles = true;
-        }
-        int moveAmount = dice1 + dice2;
-        currentPlayer.move(moveAmount);
-        return doubles;
+
+    public void rollDiceAction(button) {
+        //Add while loop for doubles and shit.
+        // ActionListener Button 'roll' rolls the dice
+        button.addActionListener((ActionEvent e) -> {
+            Player currentPlayer = Game.getCurrentPlayer();
+            Game.getDice().roll();
+            int dice1 = Game.getDice().getFirstValue();
+            int dice2 = Game.getDice().getSecondValue();
+            int moveAmount = dice1 + dice2;
+            currentPlayer.move(moveAmount);
+
+            //landOnCell();
+
+            ////endTurnButton
+        });
     }
 
+
+    public void endTurnAction(button) {
+        button.addActionListener((ActionEvent e) -> {
+            if(Game.getDice().getFirstValue()!=Game.getDice().getSecondValue()|| Game.getCurrentPlayer().getPosition()!=40) {
+                playerTurn = playerTurn++ % Game.getPlayers().size();
+            }
+            Game.setCurrentPlayer(Game.getPlayers().get(playerTurn));
+            turn();
+        });
+    }
 }
