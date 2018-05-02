20f92ac762c29a25a0a27a9933b7601a17f8a9f7
Unknown <tomchescreations@gmail.com>
2018-05-02 19:52:14 +0100

    More work on new game view

--- Source/PropertyTycoon/src/zytom/proptycoon/view/NewGameMenu/ChoosePlayers/TokenLabel.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/view/NewGameMenu/ChoosePlayers/TokenLabel.java
@@ -3,32 +3,38 @@
  */
 package zytom.proptycoon.view.NewGameMenu.ChoosePlayers;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.EnumMap;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 /**
  *
- * @author utopia
+ * @author Tom
  */
 public class TokenLabel extends JLabel {
-
-    public TokenLabel() {
-        super();
-
+    private static final String TOKEN_ICONS_PATH = "./resources/images/tokens/";
+    public enum TokenType { BOOT, SMARTPHONE, GOBLET, HATSTAND, CAT, SPOON };
+    
+    public TokenLabel(TokenType tokenType) throws IOException {
+        super(getImageIcon(tokenType));
     }
 
-    public static ImageIcon getImageIcon() {
+    public static ImageIcon getImageIcon(TokenType tokenType) throws IOException {
+        EnumMap<TokenType, String> iconMap = new EnumMap(TokenType.class);
+        iconMap.put(TokenType.BOOT, "boot.png");
+        iconMap.put(TokenType.SMARTPHONE, "smartphone.png");
+        iconMap.put(TokenType.GOBLET, "goblet.png");
+        iconMap.put(TokenType.HATSTAND, "hatstand.png");
+        iconMap.put(TokenType.CAT, "cat.png");
+        iconMap.put(TokenType.SPOON, "spoon.png");
         try {
-            return new ImageIcon(ImageIO.read(new File("path-to-file")));
+            String filepath = TOKEN_ICONS_PATH + iconMap.get(tokenType);
+            return new ImageIcon(ImageIO.read(new File(filepath)));
         } catch (IOException ex) {
-            Logger.getLogger(TokenLabel.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex;
         }
-        return null;
     }
 }
