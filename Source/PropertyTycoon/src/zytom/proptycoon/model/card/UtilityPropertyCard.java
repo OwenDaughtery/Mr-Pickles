c2e67d22c77bedcc605eb7c4d7bcc6ad2febacad
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-04 03:19:34 +0100

    Fixed getRent method in UtilityPropertyCard and added documentation

--- Source/PropertyTycoon/src/zytom/proptycoon/model/card/UtilityPropertyCard.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/model/card/UtilityPropertyCard.java
@@ -13,6 +13,14 @@ public class UtilityPropertyCard extends PropertyCard {
     private final int rentMultiplierOnSingle;
     private final int rentMultiplierOnPair;
     
+    /**
+     *
+     * @param cellRef The cell the Utility property card is associated with
+     * @param title The name of the property
+     * @param price The buy price of the property
+     * @param rentMultiplierOnSingle The cost of rent when single utility property is owned by a single player
+     * @param rentMultiplierOnPair The rent cost when both utility properties are owned by the same player
+     */
     public UtilityPropertyCard(Cell cellRef, String title, int price, int rentMultiplierOnSingle,
                                int rentMultiplierOnPair) {
         super(cellRef, title, price);
@@ -23,18 +31,19 @@ public class UtilityPropertyCard extends PropertyCard {
     /**
      * @author Zenos Pavlakou
      * @param diceValue The sum of the dice numbers rolled
-     * @param isSingle True if the utilitiy property is owned by a single player
-     * @param isPair True if both utility properties are owned by the same player
+     * @param propertyIsOwned True if the property is owned by a player
+     * @param propertyOwnerOwnsOtherUtilityProperty True if this property and the other utility property is owned by the same player
      * @return The cost of rent
      */
-    public int getRent(int diceValue, boolean isSingle, boolean isPair) {
-        if(isPair) {
-            return diceValue * this.rentMultiplierOnPair;
-        } else if(isSingle) {
+    public int getRent(int diceValue, boolean propertyIsOwned, boolean propertyOwnerOwnsOtherUtilityProperty) {
+        if(propertyIsOwned) {
+            if(propertyOwnerOwnsOtherUtilityProperty) {
+                return diceValue * this.rentMultiplierOnPair;
+            }
             return diceValue * this.rentMultiplierOnSingle;
-        } else {
-            return diceValue;
         }
+        return diceValue;
+        
     }
  
 }
