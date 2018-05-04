ac63ccfcd787eb4fd6bafb49a324ebb18f48e742
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-04 17:27:05 +0100

    Auto stash before merge of "master" and "origin/master"

--- Source/PropertyTycoon/src/zytom/proptycoon/controller/PlayerController.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/controller/PlayerController.java
@@ -99,129 +99,98 @@ package zytom.proptycoon.controller;
 //    }
 //}
 
-public class PlayerController {
-    Game game;
-    Player currentPlayer;
-
-    public PlayerController(Game game){
-        this.game = game;
-        currentPlayer = game.getCurrentPlayer();
-
-    }
-
-    public void hasLanded(Cell cell,Board board) throws CellNotFoundException, LandedOnJailException {
-
-        Board.CellType cellType = board.getCellClass(cell);
-
-        switch (cellType) {
-
-            case GO:
-                //hands off to asset management controller
-                break;
-
-            case INCOME_TAX:
-                FreeParking incomeTaxPayment = game.getFreeParking();
-                try {
-                    Transaction transaction = new Transaction(
-                            currentPlayer,
-                            incomeTaxPayment,
-                            new AssetCollection(200),
-                            new AssetCollection(0)
-                    );
-                    transaction.settleTransaction();
-                } catch (AssetOwner.AssetNotFoundException ex) {
-
-                }
-                //try transaction to pay taxman
-                break;
-            case SUPER_TAX:
-                FreeParking superTaxPayment = game.getFreeParking();
-                try {
-                    Transaction transaction = new Transaction(
-                            currentPlayer,
-                            superTaxPayment,
-                            new AssetCollection(100),
-                            new AssetCollection(0)
-                    );
-                    transaction.settleTransaction();
-                } catch (AssetOwner.AssetNotFoundException ex) {
-
-                }
-                break;
-            case JAIL:
-                throw new LandedOnJailException();
-            case POT_LUCK:
-
-                //draw a potluck card
-                break;
-            case GO_TO_JAIL:
-                currentPlayer.moveTo(40,false,game.getBank());
-                //move player to jail
-                break;
-            case FREE_PARKING:
-                //player receives free parking
-                FreeParking freeParking = game.getFreeParking();
-                try {
-                    Transaction transaction = new Transaction(
-                            freeParking,
-                            currentPlayer,
-                            new AssetCollection(game.getFreeParking().getAssetCollection().getMoney()),
-                            new AssetCollection(0)
-                    );
-                    transaction.settleTransaction();
-                } catch (AssetOwner.AssetNotFoundException ex) {
-                }
-                break;
-            case JUST_VISITING:
-                //Do nothing
-                break;
-            case STREET_PROPERTY:
-                break;
-            case STATION_PROPERTY:
-                break;
-            case UTILITY_PROPERTY:
-                break;
-            case OPPORTUNITY_KNOCKS:break;
-
-            default: throw new CellNotFoundException();
-
-
-        }
-
-}
-    public static class CellNotFoundException extends Exception {
-        public CellNotFoundException(
-        ) {
-            super (
-                    "Cell not Found"
-            );
-        }
-        /**
-         * Gets the message
-         * @return The exception message.
-         */
-        @Override
-        public String getMessage()
-        {
-            return super.getMessage();
-        }
-    }
-
-    public static class LandedOnJailException extends Exception {
-        public LandedOnJailException(
-        ) {
-            super (
-                    "Landed on jail cell"
-            );
-        }
-        /**
-         * Gets the message
-         * @return The exception message.
-         */
-        @Override
-        public String getMessage()
-        {
-            return super.getMessage();
-        }
-    }
-}
+//public class PlayerController {
+//    Player currentPlayer;
+//    Game game;
+//    public PlayerController(Player player , Game game){
+//
+//        this.currentPlayer = player;
+//        this.game = game;
+//
+//    }
+//
+//    public void hasLanded(Cell cell,Board board){
+//
+//        Board.CellType cellType = board.getCellClass(cell);
+//
+//        switch (cellType) {
+//
+//            case GO:
+//                //hands off to asset management controller
+//                break;
+//
+//            case INCOME_TAX:
+//                FreeParking incomeTaxPayment = game.getFreeParking();
+//                try {
+//                    Transaction transaction = new Transaction(
+//                            currentPlayer,
+//                            incomeTaxPayment,
+//                            new AssetCollection(200),
+//                            new AssetCollection(0)
+//                    );
+//                    transaction.settleTransaction();
+//                } catch (AssetOwner.AssetNotFoundException ex) {
+//
+//                }
+//                //try transaction to pay taxman
+//                break;
+//            case SUPER_TAX:
+//                FreeParking superTaxPayment = game.getFreeParking();
+//                try {
+//                    Transaction transaction = new Transaction(
+//                            currentPlayer,
+//                            superTaxPayment,
+//                            new AssetCollection(100),
+//                            new AssetCollection(0)
+//                    );
+//                    transaction.settleTransaction();
+//                } catch (AssetOwner.AssetNotFoundException ex) {
+//
+//                }
+//                break;
+//            case JAIL:
+//
+//                //TODO
+//                break;
+//            case POT_LUCK:
+//
+//                //draw a potluck card
+//                break;
+//            case GO_TO_JAIL:
+//                currentPlayer.moveTo(40,false,game.getBank());
+//                //move player to jail
+//                break;
+//            case FREE_PARKING:
+//                FreeParking freeParking = game.getFreeParking();
+//                try {
+//                    Transaction transaction = new Transaction(
+//                            freeParking,
+//                            currentPlayer,
+//                            new AssetCollection(game.getFreeParking().getAssetCollection().getMoney()),
+//                            new AssetCollection(0)
+//                    );
+//                    transaction.settleTransaction();
+//                } catch (AssetOwner.AssetNotFoundException ex) {
+//
+//                }
+//                //player receives free parking
+//                break;
+//            case JUST_VISITING:
+//                //Do nothing
+//                break;
+//            case STREET_PROPERTY:
+//
+//                break;
+//            case STATION_PROPERTY:
+//
+//                break;
+//            case UTILITY_PROPERTY:break;
+//            case OPPORTUNITY_KNOCKS:break;
+//
+//            default: throw new Board.CellNotFoundException();
+//
+//
+//        }
+//
+//}
+//}
