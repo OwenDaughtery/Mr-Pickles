d3270e97ed40c18f416300372b31a7d3d4b15659
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-04 01:21:49 +0100

    Edited test for DeckCreator

--- Source/PropertyTycoon/test/zytom/proptycoon/model/deckCreator/DeckCreatorTest.java
+++ Source/PropertyTycoon/test/zytom/proptycoon/model/deckCreator/DeckCreatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import zytom.proptycoon.model.Board;
 import zytom.proptycoon.model.card.CardAction;
 import zytom.proptycoon.model.card.OpportunityKnocksCard;
@@ -16,54 +17,55 @@ import zytom.proptycoon.model.card.UtilityPropertyCard;
 
 public class DeckCreatorTest {
     
+    DeckCreator deckCreator;
+    
+    @Before 
+    public void init() throws FileNotFoundException {
+        deckCreator = new DeckCreator();
+    }
+    
+    
     @Test 
     public void buildDeckCreator() throws FileNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        Assert.assertNotNull(deckCreator);
+        Assert.assertNotNull(this.deckCreator);
     }
 
     @Test
     public void createPotLuckDeck() throws FileNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        ArrayList<PotLuckCard> potLuckCards = deckCreator.createPotLuckDeck();
+        ArrayList<PotLuckCard> potLuckCards = this.deckCreator.createPotLuckDeck();
         Assert.assertEquals(16, potLuckCards.size());
 
     }
 
     @Test
     public void createOpportunityKnocksDeck() throws FileNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        ArrayList<OpportunityKnocksCard> opportunityKnocksCards = deckCreator.createOpportunityKnocksDeck();
+        ArrayList<OpportunityKnocksCard> opportunityKnocksCards = this.deckCreator.createOpportunityKnocksDeck();
         Assert.assertEquals(16, opportunityKnocksCards.size());
     }
 
     @Test
     public void createUtilityPropertyCardDeck() throws FileNotFoundException, Board.CellNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        Board board = new Board(deckCreator.getPropertyData());
+        Board board = new Board(this.deckCreator.getPropertyData());
         ArrayList<UtilityPropertyCard> utilityPropertyCards = deckCreator.createUtilityPropertyCardDeck(board);
         Assert.assertEquals(2, utilityPropertyCards.size());
     }
 
     @Test
     public void createStationPropertyCardDeck() throws FileNotFoundException, Board.CellNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        Board board = new Board(deckCreator.getPropertyData());
+        Board board = new Board(this.deckCreator.getPropertyData());
         ArrayList<StationPropertyCard> stationPropertyCards = deckCreator.createStationPropertyCardDeck(board);
         Assert.assertEquals(4, stationPropertyCards.size());
     }
 
     @Test
     public void createStreetPropertyCardDeck() throws FileNotFoundException, Board.CellNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        Board board = new Board(deckCreator.getPropertyData());
+        Board board = new Board(this.deckCreator.getPropertyData());
         ArrayList<StreetPropertyCard> streetPropertyCards = deckCreator.createStreetPropertyCardDeck(board);
         Assert.assertEquals(22, streetPropertyCards.size());
     }
 
     @Test
     public void getPropertyData() throws FileNotFoundException {
-        DeckCreator deckCreator = new DeckCreator();
-        Assert.assertEquals(40, deckCreator.getPropertyData().size());
+        Assert.assertEquals(40, this.deckCreator.getPropertyData().size());
     }
 }
