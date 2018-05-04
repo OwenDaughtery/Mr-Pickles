5d04537b421c3f00f73d8183842596d6e9d28eb9
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-04 17:25:16 +0100

    More unit tests

--- Source/PropertyTycoon/test/zytom/proptycoon/model/card/ActionCardTest.java
+++ Source/PropertyTycoon/test/zytom/proptycoon/model/card/ActionCardTest.java
@@ -3,10 +3,23 @@ package zytom.proptycoon.model.card;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class ActionCardTest {
+    
+    String descriptionOfOpportunityKnocks;
+    CardAction cardActionOfOpportunityKnocks;
+    ActionCard opportunityKnocksCard;
+    
+    @Before
+    public void init() {
+        descriptionOfOpportunityKnocks = "COLLECT £200";
+        cardActionOfOpportunityKnocks = new CardAction("GAIN", 200);
+        opportunityKnocksCard = new OpportunityKnocksCard(descriptionOfOpportunityKnocks, cardActionOfOpportunityKnocks);
+    }
 
     @Test
     public void getDescription() {
+        assertTrue("COLLECT £200".equals(opportunityKnocksCard.getDescription()));
     }
 }
