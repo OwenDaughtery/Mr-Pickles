7b03f029b07880e6cb3d0b97c740920c30158c8f
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-05 02:19:46 +0100

    Added code that ensures no more than 6 players can be created

--- Source/PropertyTycoon/src/zytom/proptycoon/controller/preGame/PlayerInitalizer.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/controller/preGame/PlayerInitalizer.java
@@ -7,14 +7,19 @@ import java.util.ArrayList;
 public class PlayerInitalizer {
 
     private ArrayList<Player> players;
+    private int numOfPlayers;
 
     public PlayerInitalizer(){
         players = new ArrayList<>();
+        numOfPlayers = 0;
     }
 
     public void createPlayer(String name, Player.TokenType tokenType){
-        Player player = new Player(name,tokenType);
-        this.players.add(player);
+        if(numOfPlayers < 6) {
+            Player player = new Player(name,tokenType);
+            this.players.add(player);
+            numOfPlayers++;
+        }
     }
 
     public ArrayList<Player> getPlayers(){
