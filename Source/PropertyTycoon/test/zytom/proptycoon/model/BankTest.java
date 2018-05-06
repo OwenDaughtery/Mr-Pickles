8e708079b8ca0323a5118887e2afd68ca3861074
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-06 02:55:46 +0100

    Added unit tests to Bank. Note: DEEPBLUE enum does not work!

--- Source/PropertyTycoon/test/zytom/proptycoon/model/BankTest.java
+++ Source/PropertyTycoon/test/zytom/proptycoon/model/BankTest.java
@@ -54,7 +54,7 @@ public class BankTest {
         ArrayList<UtilityPropertyCard>e = new ArrayList<>();
         Bank bank = new Bank(a,b,c,d,e);
 
-        System.out.print(bank.toString());
+        //System.out.print(bank.toString());
     }
 
     @Test
@@ -70,7 +70,7 @@ public class BankTest {
         players.add(player4);
 
         Game game = new Game(players,players.get(0));
-        System.out.print(game.getBank().toString());
+        //System.out.print(game.getBank().toString());
     }
 
     @Test
@@ -90,4 +90,69 @@ public class BankTest {
         bank.giveAssetCollection(asset);
         assertTrue(bank.getAssetCollection().getPotLuckCards().contains(card));
     }
+    
+    @Test
+    public void checkBrownPropertyGroup() {
+        ArrayList<StreetPropertyCard> brownProperties = bank.getGroupOfStreetProperties(Colour.BROWN);
+        assertTrue(brownProperties.size() == 2);
+        assertTrue("CRAPPER STREET".equals(brownProperties.get(0).getTitle()));
+        assertTrue("GANGSTERS PARADISE".equals(brownProperties.get(1).getTitle()));
+    }
+    
+    @Test
+    public void checkBluePropertyGroup() {
+        ArrayList<StreetPropertyCard> blueProperties = bank.getGroupOfStreetProperties(Colour.BLUE);
+        assertTrue(blueProperties.size() == 3);
+        assertTrue("WEEPING ANGEL".equals(blueProperties.get(0).getTitle()));
+        assertTrue("POTTS AVENUE".equals(blueProperties.get(1).getTitle()));
+        assertTrue("NARDOLE DRIVE".equals(blueProperties.get(2).getTitle()));
+    }
+    
+    @Test
+    public void checkGreenProperties() {
+        ArrayList<StreetPropertyCard> greenProperties = bank.getGroupOfStreetProperties(Colour.GREEN);
+        assertTrue(greenProperties.size() == 3);
+        assertTrue("SIRAT MEWS".equals(greenProperties.get(0).getTitle()));
+        assertTrue("GHENGIS CRESCENT".equals(greenProperties.get(1).getTitle()));
+        assertTrue("IBIS CLOSE".equals(greenProperties.get(2).getTitle()));
+    }
+    
+    @Test
+    public void checkOrangeProperties() {
+        ArrayList<StreetPropertyCard> orangeProperties = bank.getGroupOfStreetProperties(Colour.ORANGE);
+        assertTrue(orangeProperties.size() == 3);
+        assertTrue("COOPER DRIVE".equals(orangeProperties.get(0).getTitle()));
+        assertTrue("WOLOWITZ STREET".equals(orangeProperties.get(1).getTitle()));
+        assertTrue("PENNY LANE".equals(orangeProperties.get(2).getTitle()));
+    }
+    
+    @Test
+    public void checkPurpleProperties() {
+        ArrayList<StreetPropertyCard> purpleProperties = bank.getGroupOfStreetProperties(Colour.PURPLE);
+        assertTrue(purpleProperties.size() == 3);
+        assertTrue("SKYWALKER DRIVE".equals(purpleProperties.get(0).getTitle()));
+        assertTrue("WOOKIE HOLE".equals(purpleProperties.get(1).getTitle()));
+        assertTrue("REY LANE".equals(purpleProperties.get(2).getTitle()));
+    }
+    
+    @Test
+    public void checkRedProperties() {
+        ArrayList<StreetPropertyCard> redProperties = bank.getGroupOfStreetProperties(Colour.RED);
+        assertTrue(redProperties.size() == 3);
+        assertTrue("YUE FEI SQUARE".equals(redProperties.get(0).getTitle()));
+        assertTrue("MULAN ROUGE".equals(redProperties.get(1).getTitle()));
+        assertTrue("HAN XIN GARDEN".equals(redProperties.get(2).getTitle()));
+    }
+    
+    @Test
+    public void checkYellowProperties() {
+        ArrayList<StreetPropertyCard> yellowProperties = bank.getGroupOfStreetProperties(Colour.YELLOW);
+        assertTrue(yellowProperties.size() == 3);
+        assertTrue("KIRK CLOSE".equals(yellowProperties.get(0).getTitle()));
+        assertTrue("PICARD AVENUE".equals(yellowProperties.get(1).getTitle()));
+        assertTrue("CRUSHER CREEK".equals(yellowProperties.get(2).getTitle()));  
+    }
+    
+    // DEEPBLUE DOES NOT WORK!
+
 }
