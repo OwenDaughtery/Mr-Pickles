398035762c22206e4c0e25cc1fee88c3adf27f68
Unknown <mp441@sussex.ac.uk>
2018-05-06 22:16:44 +0100

    game controller
    Getters made

--- Source/PropertyTycoon/src/zytom/proptycoon/controller/game/GameController.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/controller/game/GameController.java
@@ -15,21 +15,71 @@ import java.util.ArrayList;
  * Creates
  */
 public class GameController {
-    Game game;
-    int playerTurn;
-    PlayerController playerController = new PlayerController(game,this);
-    PropertyLandedLogic propertyLandedLogic = new PropertyLandedLogic(game.getPlayers(),game.getBank(),this);
-    PurchasePropertyOption purchasePropertyOption = new PurchasePropertyOption(game.getBank(),this);
-    OpportunityKnocksController opportunityKnocksController = new OpportunityKnocksController(game,this,game.getBoard(),game.getBank(),game.getDice(),game.getCurrentPlayer());
-    PotLuckController potLuckController  =new PotLuckController(game,this,game.getBoard(),game.getBank(),game.getDice(),game.getCurrentPlayer());
-    DiceController diceController = new DiceController(game.getDice(),this);
-    AssetManagementController assetManagementController = new AssetManagementController(game.getBank(),this);
-    StartTurn startTurn = new StartTurn(game.getBank(),this);
-    EndTurnController endTurnController = new EndTurnController(game,this);
+    private Game game;
+    private int playerTurn;
+    private PlayerController playerController;
+    private PropertyLandedLogic propertyLandedLogic;
+    private PurchasePropertyOption purchasePropertyOption;
+    private OpportunityKnocksController opportunityKnocksController;
+    private PotLuckController potLuckController;
+    private DiceController diceController;
+    private AssetManagementController assetManagementController;
+    private StartTurn startTurn;
+    private EndTurnController endTurnController;
 
     private BoardController boardController = new BoardController(game.getBoard());
 
     public GameController(Game game) throws FileNotFoundException, Board.CellNotFoundException, Game.PlayerNumberException {
         this.game = game;
+        this.playerController = new PlayerController(this.game,this);
+        this.propertyLandedLogic = new PropertyLandedLogic(game.getPlayers(),game.getBank(),this);
+        this.purchasePropertyOption = new PurchasePropertyOption(game.getBank(),this);
+        this.opportunityKnocksController = new OpportunityKnocksController(this.game,this,game.getBoard(),game.getBank(),game.getDice(),game.getCurrentPlayer());
+        this.potLuckController = new PotLuckController(this.game,this,game.getBoard(),game.getBank(),game.getDice(),game.getCurrentPlayer());
+        this.diceController = new DiceController(game.getDice(),this);
+        this.assetManagementController = new AssetManagementController(game.getBank(),this);
+        this.startTurn = new StartTurn(game.getBank(),this);
+        this.endTurnController = new EndTurnController(this.game,this);
+    }
+
+
+    public Game getGame() {
+        return game;
+    }
+
+    public PlayerController getPlayerController() {
+        return playerController;
+    }
+
+    public PropertyLandedLogic getPropertyLandedLogic() {
+        return propertyLandedLogic;
+    }
+
+    public PurchasePropertyOption getPurchasePropertyOption() {
+        return purchasePropertyOption;
+    }
+
+    public OpportunityKnocksController getOpportunityKnocksController() {
+        return opportunityKnocksController;
+    }
+
+    public PotLuckController getPotLuckController() {
+        return potLuckController;
+    }
+
+    public DiceController getDiceController() {
+        return diceController;
+    }
+
+    public AssetManagementController getAssetManagementController() {
+        return assetManagementController;
+    }
+
+    public StartTurn getStartTurn() {
+        return startTurn;
+    }
+
+    public EndTurnController getEndTurnController() {
+        return endTurnController;
     }
 }
