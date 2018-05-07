3c95071e01076774d2f7dff04f7334130a3bfa6f
Zenos Pavlakou <zenospavlakou@outlook.com>
2018-05-07 20:45:27 +0100

    Added Income Tax png

--- Source/PropertyTycoon/src/zytom/proptycoon/view/board/cell/IncomeTaxCell.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/view/board/cell/IncomeTaxCell.java
@@ -7,12 +7,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author Tom
  */
 public class IncomeTaxCell extends InsideCell {
+    
+    private BufferedImage image;
+    
     private final String price;
     
     public IncomeTaxCell(
@@ -23,13 +33,21 @@ public class IncomeTaxCell extends InsideCell {
             String price) {
         super(boardSize, boardProportion, side, rowIndex);
         this.price = price;
+        try {
+            this.image = ImageIO.read(new File("./resources/images/cells/income_tax.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(IncomeTaxCell.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @Override
     protected void renderContents(Graphics2D g2) {
         g2.setFont(new Font("Monaco", Font.PLAIN, 8));
-        g2.drawString("Income", 4, dimension.height * 0.25f);
-        g2.drawString("Tax", 4, dimension.height * 0.25f + 10.0f);
+        g2.drawString("Income Tax", 4, dimension.height * 0.25f);
         g2.drawString("Price:  " + price, 4, dimension.height * 0.85f);
+        AffineTransform at = g2.getTransform();
+        g2.scale(0.45, 0.45);
+        g2.drawImage(image, 20, 60, null);
+        g2.setTransform(at);
     }
 }
