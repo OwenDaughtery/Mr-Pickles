49b7714bbc3600d798160078fc4addb1e6a1d2ad
Unknown <tomchescreations@gmail.com>
2018-05-07 19:03:25 +0100

    Made camera

--- Source/PropertyTycoon/src/zytom/proptycoon/view/board/BoardCamera.java
+++ Source/PropertyTycoon/src/zytom/proptycoon/view/board/BoardCamera.java
@@ -15,23 +15,63 @@ import javax.swing.JPanel;
  * @author utopia
  */
 public class BoardCamera {
-    private JPanel panel;
-    private Point position;
-    private Dimension viewport;
+    private final JPanel panel;
+    private final Point position;
+    private final Dimension viewport;
     private AffineTransform at;
+    private static final float SCALE_FACTOR = 0.5f;
+    private boolean isZoomed;
     
-    
-    public BoardCamera(JPanel panel, Dimension viewport) {
+    public BoardCamera(JPanel panel) {
         this.panel = panel;
-        this.viewport = viewport;
+        this.position = new Point(0, 0);
+        this.viewport = new Dimension(this.panel.getWidth(), this.panel.getHeight());
+        this.isZoomed = false;
+    }   
+    
+    public void zoom() {
+        if (isZoomed) return;
+        this.viewport.setSize(
+                new Dimension(
+                        Math.round((float)this.panel.getWidth() * SCALE_FACTOR),
+                        Math.round((float)this.panel.getHeight() * SCALE_FACTOR)
+                )        
+        );
+    }
+    
+    public void unzoom() {
+        if (!isZoomed) return;
+        this.viewport.setSize(
+                new Dimension(
+                        this.panel.getWidth(),
+                        this.panel.getHeight()
+                )        
+        );
+    }
+    
+    public void toggleZoom() {
+        if (isZoomed) 
+            unzoom();
+        else
+            zoom();
+    }
+    
+    public void setPosition(int x, int y) {
+        this.position.x = x;
+        this.position.y = y;
     }
     
     public void applyTransform(Graphics2D g2) {
         at = g2.getTransform();
-        g2.scale(
-                (float)this.panel.getWidth() / (float)this.viewport.width,
-                (float)this.panel.getHeight() / (float)this.viewport.height
+        g2.translate(
+                this.position.x,
+                this.position.y
         );
+        if (isZoomed)
+            g2.scale(
+                    (float)this.panel.getWidth() / (float)this.viewport.width,
+                    (float)this.panel.getHeight() / (float)this.viewport.height
+            );
     }
     
     public void clearTransform(Graphics2D g2) {
